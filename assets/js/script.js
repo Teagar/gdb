d06765089d8b2e25e6db07c8debbe7910ef767e9
Teagar <cyanofc@gmail.com>
2023-07-26 23:41:03 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+// Função para limpar a planilha
+function limparPlanilha() {
+    document.getElementById('planilha').innerHTML = '<tr><th>Data</th><th>Dia</th><th>Banca</th><th>Meta Diária</th></tr>';
+}
+
+// Função para atualizar a planilha com base na banca e meta
+function atualizarPlanilha(bancaInicial, tipoMeta, meta) {
+    const dataInicio = new Date(document.getElementById('data-inicio').value);
+    const dataFim = new Date(document.getElementById('data-fim').value);
+
+    let diasPassados = 1;
+    let bancaAtual = bancaInicial;
+
+    while (dataInicio <= dataFim) {
+        const metaDiaria = tipoMeta === 'percent' ? (bancaAtual * meta) / 100 : meta;
+        const formattedDate = dataInicio.toLocaleDateString('pt-BR'); // Formata a data em DD/MM/AA
+        const formattedBanca = `R$ ${bancaAtual.toFixed(2).replace('.', ',')}`;
+        const formattedMetaDiaria = `R$ ${metaDiaria.toFixed(2).replace('.', ',')}`;
+        const row = `<tr>
+                        <td>${formattedDate}</td>
+                        <td>${diasPassados}</td>
+                        <td class='bank'>${formattedBanca}</td>
+                        <td class='objective'>${formattedMetaDiaria}</td>
+                     </tr>`;
+        document.getElementById('planilha').insertAdjacentHTML('beforeend', row);
+        
+        diasPassados++;
+        dataInicio.setDate(dataInicio.getDate() + 1);
+        bancaAtual += metaDiaria;
+    }
+}
+
+// Evento para atualizar a planilha quando o botão "Atualizar" for clicado
+document.getElementById('atualizar-banca').addEventListener('click', function() {
+    const bancaInicial = parseFloat(document.getElementById('banca-inicial').value);
+    const tipoMeta = document.getElementById('tipo-meta').value;
+    const meta = parseFloat(document.getElementById('meta').value);
+    limparPlanilha(); // Limpa a planilha anterior
+    atualizarPlanilha(bancaInicial, tipoMeta, meta);
+});
+
+// Evento para gerar a planilha quando o botão "Gerar Planilha" for clicado
+document.getElementById('gerar-planilha').addEventListener('click', function() {
+    const bancaInicial = parseFloat(document.getElementById('banca-inicial').value);
+    const tipoMeta = document.getElementById('tipo-meta').value;
+    const meta = parseFloat(document.getElementById('meta').value);
+    limparPlanilha(); // Limpa a planilha anterior
+    atualizarPlanilha(bancaInicial, tipoMeta, meta);
+});
